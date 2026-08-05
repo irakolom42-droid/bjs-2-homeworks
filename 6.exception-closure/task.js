93b5cd56bba3dcb90f0270f2cff717305381757f
Irina I.K. <irakolom.42@yandex.ru>
2026-08-05 14:22:14 -0700

    Все задания темы выполнены

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount(parse) {
+    let parsed = Number.parseFloat(parse);
+    if (isNaN(parsed)) {
+        throw new Error('Невалидное значение');
+    } else {
+        return parsed;
+    }
+}
+
+function validateCount(value) {
+    try {
+        let result = parseCount(value);
+        return result;
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || a + c < b || c + b < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        let area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        };
+    }
+}
